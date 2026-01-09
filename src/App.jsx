0c582304536a78add56f1cb53ceb4dee1f2270ac
barlakshan <ravindu.ba.lakshan@gmail.com>
2026-01-09 09:12:47 +0530

    setup navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
@@ -5,9 +6,9 @@ gsap.registerPlugin;
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { navLinks } from "../../constants";
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+    navTween.fromTo(
+      "nav",
+      { background: "transparent" },
+      {
+        backgroundColor: "rgba(0, 0, 0, 0.50)",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
+  return (
+    <nav>
+      <a href="#home" className="flex flex-center gap-2">
+        <img src="/images/logo.png" alt="Logo" />
+        <p>Velvet Pour</p>
+      </a>
+      <ul>
+        {navLinks.map((link) => (
+          <li key={link.id}>
+            <a href={`#${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
